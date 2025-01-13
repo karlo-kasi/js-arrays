66085aacad79fa33a343a0bfe0bf189e141a42bb
karlo-kasi <karlokasi1995@gmail.com>
2025-01-13 16:08:58 +0100

    Add: BONUS

--- 02-arrays-bonus/script.js
+++ 02-arrays-bonus/script.js
@@ -26,6 +26,7 @@ console.log (longNames)
 // 3. Rimuovi 'Ed' dall'array teachers
 teachers.splice (5, 1)
 console.log (teachers)
+
 // 4. Verifica se 'Fabio' è presente nell'array teachers
 // e salva il risultato nella variabile isFabioPresent
 const isFabioPresent = teachers.includes("Fabio");
